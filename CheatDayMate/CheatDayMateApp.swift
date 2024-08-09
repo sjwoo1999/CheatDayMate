f85ae81d328b0b54f4ab0321cbde533bd71854d3
_woo_s.j <sjwoo1999@korea.ac.kr>
2024-08-10 00:52:55 +0900

    Initial Commit

---
+++ CheatDayMate/CheatDayMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheatDayMateApp.swift
+//  CheatDayMate
+//
+//  Created by 우성종 on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CheatDayMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
